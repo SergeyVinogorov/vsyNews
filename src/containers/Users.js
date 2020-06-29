b1b5c40d5c97a513d0a5e3e407d9e47247740dc6
SergeyVinogorov <vinogorich@gmail.com>
2020-06-29 22:38:12 +0300

    add api call for get news

--- src/containers/Users.js
+++ src/containers/Users.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import URL from '../apiService'
 
-const Users = () => (
-  <div>
-    <h1>The users</h1>
-    <p>Awesome users on board for this news application!</p>
-  </div>
-)
+const Users = () => {
+  const [news, setNews] = useState([])
+  useEffect(() => {
+    axios
+      .get(URL.sourcesURL + URL.apiKey)
+      .then((response) => setNews(response.data.sources))
+  }, [])
+  return (
+    <div>
+      <h1>The users</h1>
+      <p>Awesome users on board for this news application!</p>
+    </div>
+  )
+}
 export default Users
